e00710ab88d48b59cf3da1ba41d6308dc196e034
Mint Web <53662476+imintweb@users.noreply.github.com>
2020-07-04 13:23:46 +0200

    Add files via upload

---
+++ wpLSN.js
@@ -0,0 +1,2 @@
+var twLSN = ['www.elvaro.us','elvaro.us','www.cektokokita.com','cektokokita.com','tampilgaya.net','www.tampilgaya.net','localhost'];
+var redirectURL = 'https://www.seotemplate.web.id';
